891985145be3e8a0d4e89b3967255afc78f1059a
Stefan Starosta <stefan.starosta@gmail.com>
2024-05-21 09:20:46 +0200

    application spring initializer

---
+++ src/main/java/com/homeproject/sprintboardbackend/SprintBoardBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.homeproject.sprintboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBoardBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBoardBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeproject/sprintboardbackend/SprintBoardBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeproject.sprintboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBoardBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
